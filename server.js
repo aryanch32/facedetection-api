a4af29ddeecfc52c6a77d1ad18ac1d9104fcf1b2
aryanch32 <aryanch32@users.noreply.github.com>
2020-06-17 00:05:45 +0530

    added port

--- server.js
+++ server.js
@@ -24,9 +24,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {
-  res.send(database.users);
-})
+app.get('/', (req, res)=> { res.send('it is working')})
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
@@ -38,6 +36,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
